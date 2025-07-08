be8709e982b11f9828e981868e8e206e1c7a3a3c
Stackdev5 <Stackdev983@gmail.com>
2025-07-08 21:19:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
-const cors = require('cors');
 const axios = require('axios');
-const app = express();
+const cors = require('cors');
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
+const TRACKON_BOOKING_URL = 'http://trackon.in:5455/CrmApi/Crm/UploadPickupRequestWithoutDockNo';
+
 app.post('/proxy', async (req, res) => {
   try {
-    const response = await axios.post('http://trackon.in:5455/CrmApi/Crm/UploadPickupRequestWithoutDockNo', req.body);
+    const response = await axios.post(TRACKON_BOOKING_URL, req.body, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
     res.status(200).json(response.data);
   } catch (err) {
     res.status(500).json({ error: err.toString() });
   }
 });
 
-app.listen(3000, () => console.log('Proxy server running on port 3000'));
+app.get('/', (req, res) => {
+  res.send('Trackon Proxy Server Running');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
